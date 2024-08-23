d6953066a2e72da92e6993fe5fa2363adb27cb58
Aaron Wu <demonaarwu@gmail.com>
2024-08-24 07:05:44 +0800

    2024-08-24

--- lua/aaw-edit.lua
+++ lua/aaw-edit.lua
@@ -1,16 +1,16 @@
 -- aaw-edit.lua - configs for better edit
 
-require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-    },
-
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 1000,
-        lsp_format = "fallback",
-    },
-})
+-- require("conform").setup({
+--     formatters_by_ft = {
+--         lua = { "stylua" },
+--     },
+--
+--     format_on_save = {
+--         -- These options will be passed to conform.format()
+--         timeout_ms = 5000,
+--         lsp_format = "fallback",
+--     },
+-- })
 
 require("autosave").setup({
     enable = true,
@@ -19,6 +19,17 @@ require("autosave").setup({
         exists = false,
     },
 })
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
 
 require("nvim-autopairs").setup()
 
